28cb3d057376ed016dffb7e5840744f254c10dc2
SuooL <1020935219@qq.com>
2018-12-28 12:50:26 +0800

    new router

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,8 +3,9 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
     }
 
     // Example of configuring a controller
